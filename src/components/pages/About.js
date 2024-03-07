68e36747c1b8f8d63f4c053e05e68b605ba8f074
William Bailey <wlbailey0@gmail.com>
2024-03-06 21:01:53 -0600

    add to about, test bio in json

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,21 @@
 import React from "react";
+import members from "../../members/bandMembers.json"
 
 export default function About() {
     return (
-        <div>
+        <div >
             <h1>About</h1>
             <h3>Sounds of Swing</h3>
+            <li className="col s12">
+                {members.map((obj) => (
+                    <div key={obj.index} className="row">
+                        <ul className="col s12">
+                            Member {obj.name}: Instrument {obj.instrument}
+                            <ul className="col s6"> bio: {obj.bio}</ul>
+                        </ul>
+                    </div>
+                ))}
+            </li>
 
         </div>
     );
